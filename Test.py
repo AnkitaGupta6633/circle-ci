3c75c6a26c118916c395c867131604898764b597
AnkitaGupta6633 <guptaankita33324@gmail.com>
2025-09-27 18:10:49 +0530

    updatetest

--- Test.py
+++ Test.py
@@ -3,9 +3,9 @@ from main import to_upper
 
 class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
-        name = "Tannu"
+        name = "Mscit"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "TANNU")
+        self.assertEqual(upper_name, "Mscit")
 
 if __name__ == '__main__':
     unittest.main()

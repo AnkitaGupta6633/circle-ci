f14098bfc30f3261f70e047c0338254f7733d079
AnkitaGupta6633 <guptaankita33324@gmail.com>
2025-09-27 18:23:49 +0530

    Fix test case: correct expected output to uppercase

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class MyTestCase(unittest.TestCase):
     def test_to_upper(self):
         name = "Mscit"
         upper_name = to_upper(name)
-        self.assertEqual(upper_name, "Mscit")
+        self.assertEqual(upper_name, "MSCIT")   
 
 if __name__ == '__main__':
     unittest.main()
